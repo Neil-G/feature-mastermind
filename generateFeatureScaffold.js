41604c1667888fa9e216c9bc66b1ccd2635d5651
Warren Neil Gordon <neil@Warrens-MacBook-Pro.local>
2018-02-04 02:35:57 -0500

    add updating index.js

--- generateFeatureScaffold.js
+++ generateFeatureScaffold.js
@@ -2,12 +2,12 @@ const fs = require('fs')
 
 const featureSchema = {
   components: [
-    { path: ['src', 'components'], fileName: 'Example.js' },
-    { path: ['src', 'components'], fileName: 'Example2.js' }
+    { path: ['src', 'components', 'pages'], fileName: 'Example' },
+    { path: ['src', 'components', ' shared'], fileName: 'Example2' }
   ]
 }
 
-featureSchema.components.forEach(component => {
+featureSchema.components.forEach((component, i) => {
   const { path, fileName } = component
 
   // create folders if they doesn't exist
@@ -20,6 +20,22 @@ featureSchema.components.forEach(component => {
     totalPath = currentDir
   })
 
-  const pathAndFileName = totalPath + '/' + fileName
+  // create index file if not created
+  const indexFileNameAndPath = totalPath + '/' + 'index.js'
+  if (!fs.existsSync(indexFileNameAndPath)) {
+    fs.writeFileSync(indexFileNameAndPath, '')
+  }
+
+  // add export to index file
+  const exportStatement = `export { default as ${fileName} } from './${fileName}'`
+
+  let exportedComponents = fs.readFileSync(indexFileNameAndPath, 'utf8').split("\n")
+  exportedComponents.push(exportStatement)
+  exportedComponents  = [ ...new Set(exportedComponents) ].filter(statement => !!statement).join("\n")
+
+  fs.writeFileSync(indexFileNameAndPath, exportedComponents)
+
+  // create component file
+  const pathAndFileName = totalPath + '/' + fileName + '.js'
   fs.writeFileSync(pathAndFileName, '')
 })
