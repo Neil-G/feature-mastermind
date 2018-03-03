2d20c283e7b368dc1ab0ab5c2de2936b78164e03
Warren Neil Gordon <neil@Warrens-MBP.home>
2018-03-03 15:31:09 -0500

    initial commit

---
+++ featureSchemas/example.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "components": [
+    { "path" : ["pages"], "fileName": ["Example"] },
+    { "path" : ["pages"], "fileName": ["Example2"] },
+    { "path" : ["pages"], "fileName": ["Example3"] }
+  ]
+}
--- generateFeatureScaffold.js
+++ generateFeatureScaffold.js
@@ -1,18 +1,56 @@
 const fs = require('fs')
 
-const featureSchema = {
-  components: [
-    { path: ['src', 'components', 'pages'], fileName: 'Example' },
-    { path: ['src', 'components', ' shared'], fileName: 'Example2' }
-  ]
+config = {
+  pathToSrc: [ 'src' ],
+  pathToTest: [ 'test' ]
 }
+
+// const featureSchema = {
+//   name: '',
+//   description: '',
+//
+//   // TODO: this must take an toption to connect it to the store, a connectStore array
+//   components: [
+//     { path: ['pages'], fileName: 'Example' },
+//     { path: ['shared'], fileName: 'Example2' }
+//   ],
+//   snippets: [
+//
+//   ],
+//   utilities: [
+//     { path: [''], fileName: '' }
+//   ],
+//   content: [
+//
+//   ],
+//   updateSchemaCreators: [
+//
+//   ]
+// }
+
+const featureSchema = require(`./featureSchemas/${process.argv[2]}.js`)
+
+
+// TODO: turn this into a function that takes the component name and path.length for mastermind path
+const reactComponentBoilerplate = `
+import React,{ Component } from 'react'
+import mastermind from './../../mastermind'
+
+class ComponentName extends Component {
+  render() {
+    return(
+      <div className=""></div>
+    )
+  }
+}
+`
 
 featureSchema.components.forEach((component, i) => {
   const { path, fileName } = component
 
   // create folders if they doesn't exist
   let totalPath = './'
-  path.forEach(dirName => {
+  config.pathToSrc.concat(path).forEach(dirName => {
     const currentDir = totalPath + dirName + '/'
     if (!fs.existsSync(currentDir)) {
       fs.mkdirSync(currentDir)
@@ -37,5 +75,9 @@ featureSchema.components.forEach((component, i) => {
 
   // create component file
   const pathAndFileName = totalPath + '/' + fileName + '.js'
-  fs.writeFileSync(pathAndFileName, '')
+
+  if (!fs.existsSync(pathAndFileName)) {
+    console.log('file doesnt already exist')
+    fs.writeFileSync(pathAndFileName, reactComponentBoilerplate)
+  }
 })
